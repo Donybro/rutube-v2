c932c78d644fa59967072a27f325d9ff5dd3c5f5
Donybro <doniyorrahmanov957@mail.ru>
2022-09-21 10:29:25 +0500

    new models

---
+++ backend/src/user/subscription.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { Base } from "../utils/base";
+import { UserEntity } from "./user.entity";
+import { VideoEntity } from "../video/video.entity";
+
+@Entity('Subscription')
+export class SubscriptionEntity extends Base{
+
+  @ManyToOne(()=>UserEntity,user=>user.subscriptions)
+  @JoinColumn({name:"from_user_id"})
+  fromUser:UserEntity[]
+
+  @OneToMany(()=>UserEntity,user=>user.subscriptions)
+  @JoinColumn({name:"to_channel_id"})
+  toChannel:UserEntity[]
+
+
+}
