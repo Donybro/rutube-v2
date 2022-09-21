cc474fe8aaab60d920b1e0c104bf6ca1928b597f
Donybro <doniyorrahmanov957@mail.ru>
2022-09-21 17:55:51 +0500

    user service done

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -8,7 +8,7 @@ export const getTypeOrmConfig = async (
 	host: 'localhost',
 	port: configService.get('PORT'),
 	database: configService.get('DATABASE'),
-	username: configService.get('USERNAME'),
+	username: 'postgres',
 	password: configService.get('PASSWORD'),
 	autoLoadEntities: true,
 	synchronize: true
---
+++ backend/src/user/user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsString } from 'class-validator'
+import { JoinColumn } from 'typeorm'
+
+export class UserDto {
+	@IsEmail()
+	email: string
+
+	@IsString()
+	password: string
+
+	@IsString()
+	name: string
+
+	@IsString()
+	description: string
+
+	@IsString()
+	@JoinColumn({ name: 'avatar_path' })
+	avatarPath: string
+}
